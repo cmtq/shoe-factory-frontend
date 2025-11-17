d97183092df5c08c0c39239c07fe3cfb7b508e92
cmtq <cmtq3@proton.me>
2025-11-17 16:18:12 +0000

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Category, Product, Order, Inventory } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://shoe-factory-backend-zjnvqw-e52790-185-230-64-201.traefik.me/api/',
+  baseURL: process.env.VITE_API_URL
   headers: {
     'Content-Type': 'application/json',
   },

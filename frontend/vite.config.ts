b8567ecb65e67f55d02fc484c302f05b67b7a2d6
cmtq <cmtq3@proton.me>
2025-11-17 17:02:53 +0000

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     proxy: {
       '/api': {
         target: 'http://shoe-factory-backend-zjnvqw-e52790-185-230-64-201.traefik.me',
